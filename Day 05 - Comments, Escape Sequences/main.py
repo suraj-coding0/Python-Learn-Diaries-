7c3db26f01b52f41337db80c0210c97db5b3ec46
Suraj Maurya <surajmaurya0706@gmail.com>
2024-09-19 17:00:09 +0530

    Python

---
+++ Day 05 - Comments, Escape Sequences/main.py
@@ -0,0 +1,11 @@
+'''
+hey suraj please don't remove this line 
+author:suraj
+course:100days of code
+'''
+
+print("I Am A Good Boy")
+print("my name is \"suraj\" maurya\ni am pursuing BCA 3rd year")
+#this is single line comment
+print("this is print statement.")
+print("hello world!!!")   #this is comment
---
+++ Day 06 - Variables & DataTypes/main.py
@@ -0,0 +1,25 @@
+a = 18
+b = True
+c = "suraj"
+d = None
+a1 = 9.5
+print(a)
+print(b)
+print(c)
+print(d)
+print(a1)
+print(a + a1)
+print("the type of a is", type(a))
+print("the type of a is", type(b))
+print("the type of a is", type(c))
+print("the type of a is", type(d))
+print("the type of a is", type(a1))
+
+list = [18, 2.3, [-4, 6], ["apple", "banana"]]
+print(list)
+
+tuple = (("parrot", "sparrow"), ("lion", "tiger"))
+print(tuple)
+
+dict = {"name": "suraj", "age": 20, "canvote": True}
+print(dict)
