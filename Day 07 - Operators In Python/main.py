4e95558a0d52fdd9d3a03442dcdc2836a811127d
Suraj Maurya <surajmaurya0706@gmail.com>
2024-09-20 15:30:41 +0530

    Python

---
+++ Day 07 - Operators In Python/main.py
@@ -0,0 +1,7 @@
+print(15+7)          #Addition operator
+print(15-7)          #Subtraction operator
+print(15*7)          #Multiplication
+print(15/7)          #Divsion
+print(55//7)         #Floor division
+print(5**3)          #Exponential
+print(40%3)          #Modulus
---
+++ Day 08 - Make Simple Calculater/main.py
@@ -0,0 +1,8 @@
+num1 = int(input("enter you first no."))
+num2 = int(input("enter you second no."))
+
+
+print("the value of", num1, "+", num2, "is:", num1 + num2)
+print("the value of", num1, "-", num2, "is:", num1 - num2)
+print("the value of", num1, "*", num2, "is:", num1 * num2)
+print("the value of", num1, "/", num2, "is:", num1 / num2)
