b1b0195e388f371563d27b8779ea9f3bd26b827d
Suraj Maurya <surajmaurya0706@gmail.com>
2024-09-23 22:30:36 +0530

    Python

---
+++ Day 12 - Strings Slicing and Operations/main.py
@@ -0,0 +1,27 @@
+name = ("suraj maurya")
+print(name[0:5])
+
+# fruit = "Mango apple"
+# len = (len(fruit))
+# print("Mango is a", len, "letter word.")
+
+pie = "ApplePie"
+print(pie[:5])
+print(pie[6])	      #returns character at specified index
+
+fruit = "Mango"
+mangoLen = len(fruit)
+print(mangoLen)
+
+print(fruit[0:4])        # including 0 but not 4
+print(fruit[1:4])        # including 1 but not 4
+print(fruit[:5])
+print(fruit[0:-3])
+print(fruit[0 :len(fruit) - 3])
+print(fruit[-1:len(fruit) - 3])
+print(fruit[-3:-1])
+
+
+# Quick Quiz:
+nm = "Suraj"
+print(nm[-4:-2])
