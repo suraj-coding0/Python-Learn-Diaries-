91eddca6ce9b4e41a717e78897c20c9ad70185e5
Suraj Maurya <surajmaurya0706@gmail.com>
2024-09-25 23:47:24 +0530

    Python

---
+++ Day 14 - Conditions In Pthon/my-if.py
@@ -0,0 +1,18 @@
+#If-else statement
+
+a = int(int(input("Enter Your Age:")))
+print("your age is:", a)
+"""
+conditional operators
+>, <, >=, <=, ==, !=
+print(a>18)
+print(a<18)
+print(a>=18)
+print(a<=18)
+print(a==18)
+print(a!=18)
+"""
+if (a > 18):
+  print("you can drive")
+else:
+  print("you cannot drive")
