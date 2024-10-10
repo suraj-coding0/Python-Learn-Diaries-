b4774fe04ae51b5b92fa776d4068d8098fffba14
Suraj Maurya <surajmaurya0706@gmail.com>
2024-10-10 22:25:27 +0530

    Python

---
+++ Day 41 - Short Hand If Else In Python/main.py
@@ -0,0 +1,16 @@
+# If-Else Property
+x = 18
+y = 20
+
+if x > y:
+    print("X is Greater")
+elif x == y:
+    print("X Is Equal To Y")
+else:
+    print("Y Is greater")
+
+
+# Short Hand If Else Property
+a = 18 
+b = 30
+print("A Is Greater") if a > b else print("A Is Equal To B") if a==b else print("B Is Greater") 
---
+++ Day 42 - Enumerate Function in Python/main.py
@@ -0,0 +1,31 @@
+marks = [12, 56, 32, 98, 12,  45, 1, 4]
+
+# index = 0
+# for mark in marks:
+#   print(mark)
+#   if(index == 3):
+#     print("Harry, awesome!")
+#   index +=1
+
+for index, mark in enumerate(marks, start=1):
+  print(index, mark)
+  if(index == 3):
+    print("Harry, awesome!")
+
+
+
+# Loop over a list and print the index and value of each element
+fruits = ['apple', 'banana', 'mango']
+for index, fruit in enumerate(fruits):
+    print(index, fruit)
+
+
+fruits = ['apple', 'banana', 'mango']
+for index, fruit in enumerate(fruits):
+    print(f'{index+1}: {fruit}')
+
+
+# Loop over a string and print the index and value of each character
+s = 'hello'
+for index, c in enumerate(s):
+    print(index, c)
