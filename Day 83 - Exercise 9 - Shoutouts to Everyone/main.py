e30c45b86b63ce4e4c1f6c3eb9aa27e8dffb16d2
Suraj Maurya <surajmaurya0706@gmail.com>
2024-11-01 19:24:04 +0530

    Python

---
+++ Day 83 - Exercise 9 - Shoutouts to Everyone/main.py
@@ -0,0 +1,13 @@
+import pyttsx3 as py
+
+names = []
+while True:
+    name = input("Enter A Name For Shoutout And 0 For Quit: ")
+    if name.lower() == '0':         #when you are quit the program then shout out the names
+        break
+    names.append(name)
+
+for name in names:
+    engine = py.init()
+    engine.say(f"Shoutout to {name}")
+    engine.runAndWait()
