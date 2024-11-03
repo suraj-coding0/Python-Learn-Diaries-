94e7110ff8c51c20efff6801413dafe4564964e1
Suraj Maurya <surajmaurya0706@gmail.com>
2024-11-04 00:08:54 +0530

    Python

---
+++ Day 87 - Shutil Module in Python/main.py
@@ -0,0 +1,11 @@
+import shutil
+import os
+# shutil.copy('main.py', "main2.py")
+
+# shutil.copy('main.py', "main2.py")
+
+# shutil.copytree(".tutorial", "mytutorial")
+
+# shutil.move('.tutorial/text.txt', "text.txt")
+
+# os.remove("text.txt")
