46857846bd845b917a9900750677a076dad96f8c
Suraj Maurya <surajmaurya0706@gmail.com>
2024-11-09 12:57:41 +0530

    Update main.py

--- Day 94 - Exersice 11 - Drink Water Reminder/main.py
+++ Day 94 - Exersice 11 - Drink Water Reminder/main.py
@@ -5,4 +5,3 @@ if __name__=="__main__":
     while True:
         notification.notify(title ='Please drink water',app_name="Drinking water" ,message='Please Drink Water And Maintain Your Health',timeout=5 )
         time.sleep(7200)
-        
